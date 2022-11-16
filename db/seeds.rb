32d6094d31bcfa7fb725bae600758ea9afac706e
3433 <3433@example.com>
2022-11-16 11:20:43 +0900

    微調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+def self.reset_pk_sequense(table_name)
+    case ActiveRecord::Base.connection.adapter_name
+    when 'SQLite'
+        ActiveRecord::Base.connection.execute("delete from sqlite_sequence where name='#{table_name}'")
+    when 'PostgreSQL'
+        ActiveRecord::Base.connection.execute("select setval('#{table_name}_id_seq',1,false)")
+    else
+        raise "Task not implemented for this DB adapter"
+    end
+end
+
+District.delete_all
+reset_pk_sequense("districts")
+
+CSV.foreach("db/districts.csv",headers:true) do |row|
+    District.create(
+        name: row["name"]
+    )
+end
